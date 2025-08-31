a01a350130d5704d6b7fbe5a8598307eab51fda9
LIPUN ROUT <lipunrout001@gmail.com>
2025-08-31 23:46:54 +0530

    update

--- PrintSubArray.java
+++ PrintSubArray.java
@@ -1,17 +1,27 @@
 public class PrintSubArray {
   public static void subArray(int[] arr) {
+
+    int currentSum = 0;
+    int maxSum = Integer.MIN_VALUE;
     for (int i = 0; i < arr.length; i++) {
       for (int j = i; j < arr.length; j++) {
+        currentSum = 0;
         for (int k = i; k <= j; k++) {
-          System.out.print(arr[k] + " ");
+          currentSum += arr[k];
         }
-        System.out.println();
+        System.out.println(currentSum);
+        if (maxSum < currentSum) {
+          maxSum = currentSum;
+        }
+        System.out.println("current sum"+currentSum);
       }
-      System.out.println();
+
     }
+    System.out.println("Maximum Sum = " + maxSum);
   }
+
   public static void main(String[] args) {
-    int[] arr = { 1, 2, 3, 4, 5 };
+    int[] arr = { 2, 4, 6, 8, 10 };
     subArray(arr);
   }
 }
